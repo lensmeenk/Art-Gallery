262dc3a37cbe58db35f7d4187e29d3d24db47bbf
lensmeenk <lensmeenk@hotmail.com>
2017-01-18 11:02:28 +0100

    aaaaaaaaaaaa

---
+++ Assets/scripts/video.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class video : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+    }
+}
